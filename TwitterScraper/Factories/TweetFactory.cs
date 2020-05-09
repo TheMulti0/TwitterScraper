bcb4a9d86b8586f3ae6e261f78e3cc9b6edf30ed
TheMulti0 <multi@codeprecise.com>
2020-05-09 17:14:51 +0300

    Tweet interactions count

--- TwitterScraper/Factories/TweetFactory.cs
+++ TwitterScraper/Factories/TweetFactory.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 using HtmlAgilityPack;
 
 namespace TwitterScraper
 {
     public static class TweetFactory
     {
-        private static DateTime _unixEpoch = new DateTime(1970, 1, 1);
+        private static readonly DateTime UnixEpoch = new DateTime(1970, 1, 1);
+        private const string InteractionPattern = "[a-zA-Z]+";
+        private const string NumberPattern = "[0-9]+";
 
         public static Tweet CreateTweet(HtmlNode tweet, HtmlNode profile)
         {
@@ -16,26 +19,37 @@ namespace TwitterScraper
             Author author = AuthorFactory.CreateAuthor(profile);
             
             var relativeUrl = profile.GetAttributeValue("data-permalink-path", null);
+
+            string retweetId = profile.GetAttributeValue("date-retweet-id", null);
+            var isRetweet = retweetId != null;
             
-            var date = GetDate(tweet);
-            
+            var publishDate = GetPublishDate(tweet);
 
             var text = tweet.FirstDescendantWithClassOrDefault("tweet-text").InnerText;
 
             var interactions = tweet.Descendants()
-                .Where(node => node.HasClass("ProfileTweet-actionCountForAria"));
+                .Where(node => node.HasClass("ProfileTweet-actionCountForAria"))
+                .Select(node => node.InnerText)
+                .ToDictionary(
+                    s => Regex.Match(s, InteractionPattern).Value,
+                    s => int.Parse(
+                        Regex.Match(s.Replace(",", ""), NumberPattern).Value));
 
             return new Tweet
             {
                 Id = id,
                 Author = author,
                 Url = $"{TwitterConstants.BaseAddress}{relativeUrl}",
-                PublishDate = date,
-                Text = text
+                IsRetweet = isRetweet,
+                PublishDate = publishDate,
+                Text = text,
+                RepliesCount = interactions["replies"],
+                RetweetsCount = interactions["retweets"],
+                LikesCount = interactions["likes"]
             };
         }
         
-        private static DateTime GetDate(HtmlNode tweet)
+        private static DateTime GetPublishDate(HtmlNode tweet)
         {
             string ticksString = tweet
                 .FirstDescendantWithClassOrDefault("_timestamp")
@@ -43,7 +57,7 @@ namespace TwitterScraper
 
             long ticks = long.Parse(ticksString);
             
-            return _unixEpoch.AddMilliseconds(ticks).ToLocalTime();
+            return UnixEpoch.AddMilliseconds(ticks).ToLocalTime();
         }
     }
 }
